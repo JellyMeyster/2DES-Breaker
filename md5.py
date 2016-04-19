ab8e874da30ef6e80631d728b204e89870341789
bhealy <bhealy@unomaha.edu>
2016-04-18 19:01:17 -0500

    sha and md5 now take in user input

--- md5.py
+++ md5.py
@@ -1,8 +1,12 @@
 import hashlib
-lines = [line.rstrip('\n') for line in open('your password file path')]
-hashout  = open("your output file path", "w")
-for line in lines:            
-    m = hashlib.md5()
-    m.update(line)
-    hashout.write(m.hexdigest())
-    hashout.write("\n")
+import sys
+if len(sys.argv) != 3:
+	print "Provide the filename containing each string and an output filename"
+else:
+	lines = [line.rstrip('\n') for line in open(sys.argv[1])]
+	hashout  = open(sys.argv[2], "w")
+	for line in lines:            
+	    m = hashlib.md5()
+	    m.update(line)
+	    hashout.write(m.hexdigest())
+	    hashout.write("\n")
