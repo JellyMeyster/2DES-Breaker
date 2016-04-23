10dd2877df3c2245b6ab9a8acfae7988f5cd0c6f
bhealy <bhealy@unomaha.edu>
2016-04-23 17:07:01 -0500

    brute forces all keys

--- meetInTheMiddle.py
+++ meetInTheMiddle.py
@@ -1,22 +1,22 @@
 import subprocess
 import sys
 import itertools
+import gDES
 
-if len(sys.argv) !=2:
-  print "Please give the plaintext"
-else:
-	PT = sys.argv[1]	
-	proc=subprocess.Popen('python gDES.py ' + PT + ' encrypt 2', shell=True, stdout=subprocess.PIPE, )
-	CT=proc.communicate()[0].strip('\n')
-	print CT
+Khex = ['FEDCBA9876543210', 'FEDCBA9876543211'] # keys for both DES iterations
+PT = '6100000000000000'
 
-	keyChars = '0123456789abcdef'
-	keyLen = 16
-	gen = itertools.product(keyChars, repeat=keyLen)
-	for keyvals in gen:
-		key = ''.join(keyvals)
-		print key
-		'''res=subprocess.Popen('python gDES.py ' + PT + ' encrypt 1 ' + key, shell=True, stdout=subprocess.PIPE, )
-		print res.coomunicate()[0].strip('\n')'''
-		'''res=subprocess.Popen('python gDES.py ' + CT + ' decrypt 1 ' + key, shell=True, stdout=subprocess.PIPE, )
-		print res.coomunicate()[0].strip('\n')'''
+print PT
+r = gDES.DESstuff(PT, Khex[0], 'encrypt') + ' '
+CT = gDES.DESstuff(r, Khex[1], 'encrypt') + ' '
+print CT
+
+
+keyChars = '0123456789abcdef'
+keyLen = 16
+gen = itertools.product(keyChars, repeat=keyLen)
+for keyvals in gen:
+	key = ''.join(keyvals)
+	#print key
+	print "ENCRYPT: " + gDES.DESstuff(PT, key, 'encrypt') + "  :  " + key
+	print "DECRYPT: " + gDES.DESstuff(CT, key, 'decrypt') + "  :  " + key
