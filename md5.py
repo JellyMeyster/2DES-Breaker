80f2be5510571a6a3fcdd3bf26fbdfaa70928404
Seth Hanson <sethhanson@unomaha.edu>
2016-04-27 04:17:20 -0500

    Includes salt and accounts for password length

--- md5.py
+++ md5.py
@@ -1,12 +1,26 @@
 import hashlib
 import sys
-if len(sys.argv) != 3:
-	print "Provide the filename containing each string and an output filename"
+from os import urandom
+
+salt = ""
+salt_length = 0
+pass_length = 0
+diff = 0
+
+if len(sys.argv) != 5:
+	print "Provide the filename containing each string, an output filename and a salt if applicable."
 else:
+	go_salt = sys.argv[3].rstrip('\n')
+	pass_length = int(sys.argv[4].rstrip('\n'))
 	lines = [line.rstrip('\n') for line in open(sys.argv[1])]
 	hashout  = open(sys.argv[2], "w")
-	for line in lines:            
-	    m = hashlib.md5()
-	    m.update(line)
-	    hashout.write(m.hexdigest())
-	    hashout.write("\n")
+	for line in lines:
+		if go_salt == "y":
+			if len(line) < pass_length:
+				diff = pass_length - len(line)
+				salt_length =  diff/2     
+			salt = urandom(salt_length).encode('hex')
+		m = hashlib.md5() 
+		m.update(line + salt)
+		hashout.write(m.hexdigest())
+		hashout.write("\n")
